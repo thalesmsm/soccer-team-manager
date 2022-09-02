1a9e4dc9b7a022ff8e0e30472c0f5efcc59366c7
Thales Maciel <thalesmsm12@gmail.com>
2022-09-02 14:49:45 -0300

    cria rotas CRUD

--- src/app.js
+++ src/app.js
@@ -1,5 +1,77 @@
 const express = require('express');
+const validateTeam = require('./middlewares/validateTeam');
 
-const app = express();            
+const app = express();
+app.use(express.json());
+
+const teams = [
+  {
+    id: 1,
+    name: 'São Paulo Futebol Clube',
+    initials: 'SPF',
+  },
+  {
+    id: 2,
+    name: 'Clube Atlético Mineiro',
+    initials: 'CAM',
+  },
+  {
+    id: 3,
+    name: 'Bahia',
+    initials: 'BAH',
+  },
+];
+
+const existingId = (req, res, next) => {
+  const id = Number(req.params.id);
+
+  if (teams.some((t) => t.id === id)) {
+    return next();
+  }
+  res.sendStatus(404);
+};
+
+app.get('/', (req, res) => res.status(200).json({ message: 'Olá Mundo!' }));
+app.get('/teams', (req, res) => res.status(200).json(teams));
+
+app.get('/teams/:id', existingId, (req, res) => {
+  const id = Number(req.params.id);
+  const team = teams.find((t) => t.id === id);
+  res.json(team);
+});
+
+app.post('/teams', validateTeam, (req, res) => {
+  const newTeam = { ...req.body };
+  teams.push(newTeam);
+
+  res.status(201).json({ team: newTeam });
+});
+
+app.put('/teams/:id', validateTeam, existingId, (req, res) => {
+  console.log(req);
+  const { id } = req.params;
+  const { name, initials } = req.body;
+  let updatedTeam;
+
+  for (let i = 0; i < teams.length; i += 1) {
+    const team = teams[i];
+
+    if (team.id === Number(id)) {
+      team.name = name;
+      team.initials = initials;
+      updatedTeam = team;
+    }
+  }
+
+  res.status(200).json({ updatedTeam });
+});
+
+app.delete('/teams/:id', (req, res) => {
+  const { id } = req.params;
+  const arrayPosition = teams.findIndex((team) => team.id === Number(id));
+  teams.splice(arrayPosition, 1);
+
+  res.status(200).end();
+});
 
 module.exports = app;
